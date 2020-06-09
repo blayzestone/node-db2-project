08580fcdd65cbfe6b2d60f4fe9a505169c6719df
Blayze <blayzestone@protonmail.com>
2020-06-09 13:10:20 -0700

    add CREATE/READ endpoints to /api/cars

--- api/carsRouter.js
+++ api/carsRouter.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const db = require("../data/connection");
 
 module.exports = router;
 
 router.get("/", (req, res) => {
-  res.status(200).json({ msg: "up" });
+  db("cars")
+    .select("*")
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  db("cars")
+    .insert(req.body)
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
