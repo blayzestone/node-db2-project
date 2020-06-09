2f1da72cea46bc7b945b7d75971546aaf9d2167d
Blayze <blayzestone@protonmail.com>
2020-06-09 13:01:23 -0700

    project setup

---
+++ data/migrations/20200609124136_init.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments();
+    tbl.string("vin").notNullable().unique();
+    tbl.string("make").notNullable();
+    tbl.string("model").notNullable();
+    tbl.integer("mileage").notNullable();
+    tbl.string("transmission");
+    tbl.string("title");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("cars");
+};
